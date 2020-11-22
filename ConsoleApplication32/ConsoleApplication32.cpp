5a0a0027876b7bf404ffffae10c29ba2e76753a1
MorawskiR <r.morawski92@gmail.com>
2020-11-22 18:28:19 +0100

    22.11.2020

--- ConsoleApplication32/ConsoleApplication32.cpp
+++ ConsoleApplication32/ConsoleApplication32.cpp
@@ -1,8 +1,33 @@
 ﻿
 
 #include <iostream>
+#include "Time.h"
 
 int main()
 {
-	
+	using std::cout;
+	using std::endl;
+
+	Time planning;
+	Time coding(2, 40);
+	Time fixing(5, 55);
+	Time total;
+
+	cout << "czas planowania = ";
+	planning.Show();
+	cout << endl;
+
+	cout << "czas kodowania = ";
+	coding.Show();
+	cout << endl;
+
+	cout << "czas poprawiania = ";
+	fixing.Show();
+	cout << endl;
+
+	total = coding.Sum(fixing);
+	cout << "łącznie (coding.Show(fixing) = ";
+	total.Show();
+	cout << endl;
+	return 0;
 }
